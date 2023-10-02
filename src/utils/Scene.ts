c5cc817a97965f9b9abbeba5adc64f443bb992fc
王moumougit <2063735921@qq.com>
2023-10-02 14:27:19 +0800

    fix

--- src/utils/Scene.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-function createScene() {
-  const scene = new THREE.Scene()
-
-  return scene
-}
-
-export { createScene }
--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,15 +1,5 @@
 <template>
-  <div ref="sceneRef" style="width: 100vw; height: 100vh"></div>
+  <div style="width: 100vw; height: 100vh"></div>
 </template>
 
-<script lang="ts" setup>
-import { onMounted, ref } from 'vue'
-import { World } from '~/utils/World'
-const sceneRef = ref<HTMLElement>()
-
-onMounted(async () => {
-  const world = new World(sceneRef.value!)
-  await world.loadOutsideModel()
-  world.render()
-})
-</script>
+<script lang="ts" setup></script>
